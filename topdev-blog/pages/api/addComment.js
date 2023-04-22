9ec998d507d2287d10b6eb776c7b786a57ac6060
Sebabin <sebastiancrespin279@gmail.com>
2023-04-21 18:35:19 -0600

    I added the addLike route and have tested it and all seems to be going well I also had to make some changes in the addComment.js file/route as when I put the dbConnect in the try block I accidentaly discarded the all important prhase {await}

--- topdev-blog/pages/api/addComment.js
+++ topdev-blog/pages/api/addComment.js
@@ -1,18 +1,16 @@
 import dbConnect from "@/db/config/connection";
-import mongoose from "mongoose";
 // create comment giving it the user _id and the blogpost _id
 // then push the comment to the blogpost, find it by _is and update
 import {Comment,BlogPost} from '@/db/models';
 
 export default async function addComment ({body},res) {
-    console.log(body);
+    let  comment;
     try{
-    dbConnect();
+    await dbConnect();
     }catch(e) {
         console.log(e);
         res.status(500).json({message:'Error connecting to the database'});
     }
-    let  comment;
     if(!body) {
        res.status(500).json({message: 'Internal server error with Comment'}); 
     }
@@ -30,7 +28,7 @@ export default async function addComment ({body},res) {
     {new: true}
    ).populate('comments');
 
-   console.log(updatedPost);
+   
 
    res.status(200).json(updatedPost);
     }
---
+++ topdev-blog/pages/api/addLike.js
@@ -0,0 +1,32 @@
+import dbConnect from "@/db/config/connection";
+import {Like,BlogPost} from '@/db/models';
+
+export default async function addLike({body},res) {
+    let like;
+    try{
+        await dbConnect();
+    }catch(e){
+        console.log(e);
+        res.status(500).json({message:'Error with database'});
+    };
+
+    try{
+        like = await Like.create({user:body.user});
+    }catch(e){
+       console.log(e);
+       res.status(500).json({massage:'error with like'}); 
+    }
+
+    try{
+        const updatedPost = await BlogPost.findByIdAndUpdate(
+            {_id: body.blogPost},
+            {$push: {likes: like}},
+            {new: true}
+            
+            ).populate('likes');
+        res.status(200).json(updatedPost)
+    }catch(e){
+        console.log(e);
+        res.status(500).json({message:'error with blogpost'});
+    }
+}
