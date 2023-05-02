0b4033bc4b0a184fb46a54aa300f1d387331e8fd
Sebabin <sebastiancrespin279@gmail.com>
2023-05-02 14:58:12 -0600

    added some new styles and media queries as well as a hero image to the about page

--- topdev-blog/pages/About.js
+++ topdev-blog/pages/About.js
@@ -3,7 +3,16 @@ import styles from '@/styles/Home.module.css';
 const About = () => {
     return(
        <>
-       <section className="d-flex flex-row">
+       <section className={styles.hero}>
+        <Image
+          className="rounded-end"
+          src='/anime-me.png'
+          alt="AI version of topdevs fearless founder"
+          sizes="(max-width: 2560px) 22vw,"
+          fill
+        />
+       </section>
+       <section className="d-flex flex-row mt-5">
         <div className={styles.aboutImg}>
         <Image
         className="rounded-end"
@@ -13,12 +22,12 @@ const About = () => {
         fill
         />
         </div>
-        <p className="w-50 p-2 border border-primary">
-         TopDev blog is a great place to go if you want to read about the latest tech news and trends
+        <p className="w-50 p-2 p-sm-5 border border rounded-start text-white font-weight-bold shadow-lg">
+         TopDev blog is a great place to go if you want to read about tech news and trends
         </p>
        </section>
-       <section className="d-flex flex-row">
-        <p className="w-50 p-2 border border-dark">
+       <section className="d-flex flex-row mt-5 ">
+        <p className="w-50 p-2 border border rounded-end p-sm-5 text-white font-weight-bold shadow-lg">
             At TopDev blog we focus on websites web apps as well as all things tech. 
         </p>
         <div className={styles.aboutImg}>
@@ -31,7 +40,7 @@ const About = () => {
         />
         </div>
        </section>
-       <section className="d-flex flex-row">
+       <section className="d-flex flex-row mt-5 mb-5">
        <div className={styles.aboutImg}>
         <Image
         className="rounded-end"
@@ -41,7 +50,7 @@ const About = () => {
         fill
         />
         </div>
-        <p className="w-50 p-2 border border">
+        <p className="w-50 p-2 border border rounded-start p-sm-5 text-white shadow-lg">
         Along with the effects this has on
         our world our busineses and lives.
         </p>
