e207eea2ee8927167cc81beece27ed9f1bf4842a
Sebabin <sebastiancrespin279@gmail.com>
2023-05-25 19:44:16 -0600

    added topdev logo cube to the the webapp its lighter in color than I want but its at least working

--- topdev-blog/components/ThreeDLogo.js
+++ topdev-blog/components/ThreeDLogo.js
@@ -1,9 +1,12 @@
 import { PerspectiveCamera } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { useRef } from "react";
 import styles from '../styles/Home.module.css';
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
 function Cube() {
     const meshRef = useRef(null);
+
+    const logo = useLoader(TextureLoader, 'new-td-logo.png');
   
     useFrame(() => {
       if (!meshRef.current) {
@@ -17,8 +20,8 @@ function Cube() {
     return (
       <mesh ref={meshRef}>
         
-        <boxGeometry args={[4,4,4]}/>
-        <meshStandardMaterial color="blue" />
+        <boxGeometry args={[555,555,555]}/>
+        <meshStandardMaterial toneMapped={false}  mapScale={0.2} map={logo}  />
       </mesh>
     );
   }
@@ -29,7 +32,7 @@ return(
     <Canvas
     camera={{
       
-        position: [0,0,5],
+        position: [0,0,1000],
       }}>
         <ambientLight />
       <pointLight position={[10, 10, 10]} />
